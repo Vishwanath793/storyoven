421d0a2824382a33ba446eada9dba515d80e3f41
Vishwa <vishwanath793@gmail.com>
2023-08-07 10:51:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import JobListingsPage from './Components/JobListingsPage'
+
+function App() {
+  return (
+    <div className='App'>
+      <JobListingsPage />
+    </div>
+  )
+}
+
+export default App
